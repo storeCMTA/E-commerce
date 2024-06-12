eec67398a75a9d4d21b8f5388ff3e1226dd3b398
Mariem Chebbi <chebbimariem8888@gmail.com>
2024-06-12 19:20:59 +0100

    finale changes1

--- server/routes/routers.js
+++ server/routes/routers.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const ProductsController = require('../controller/contoller.js')
  
-router.post('/addProduct', ProductsController.addProduct)
-router.get('/getProducts', ProductsController.getProducts)
-router.patch('/updateProduct/:_id',ProductsController.updateProduct)
-router.delete('/deleteProduct/:_id',ProductsController.deleteProduct)
+router.post('/addProduct', ProductsController.addProduct);
+router.get('/getProducts', ProductsController.getProducts);
+router.patch('/updateProduct/:_id',ProductsController.updateProduct);
+router.delete('/deleteProduct/:_id',ProductsController.deleteProduct);
 
-module.exports = router;
+module.exports = router; 
