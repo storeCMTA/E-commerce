dde76f40901c277fadb82a1db58b95d034099138
unknown <shaymabahi330@gmail.com>
2024-06-17 21:54:43 +0100

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import AllProducts from './components/AllProducts';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <div className="App">
      <AllProducts/>
+     
     </div>
   );
 }
