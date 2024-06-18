e09d32461f75150eccd3b494287e0e60b30937ee
Mariem Chebbi <chebbimariem8888@gmail.com>
2024-06-18 14:05:15 +0100

    images in assets

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -10,11 +10,11 @@ const Home = ({products}) => {
         <div className="container mx-auto px-4 py-4 flex justify-between items-center">
           <div className="text-xl font-bold">ClothingShop</div>
           <div className="hidden md:flex space-x-4">
-            <Link to="/" className="text-gray-600 hover:text-gray-800">Home</Link>
-            <Link to="/shop" className="text-gray-600 hover:text-gray-800">Shop</Link>
-            <Link to="/about" className="text-gray-600 hover:text-gray-800">About</Link>
-            <Link to="/contact" className="text-gray-600 hover:text-gray-800">Contact</Link>
-            <Link to="/login" className="text-gray-600 hover:text-gray-800">Login</Link>
+            <Link to="/" className="text-gray-600 hover:text-gray-900">Home</Link>
+            <Link to="/shop" className="text-gray-600 hover:text-gray-900">Shop</Link>
+            <Link to="/about" className="text-gray-600 hover:text-gray-900">About</Link>
+            <Link to="/contact" className="text-gray-600 hover:text-gray-900">Contact</Link>
+            <Link to="/login" className="text-gray-600 hover:text-gray-900">Login</Link>
           </div>
         </div>
       </nav>
